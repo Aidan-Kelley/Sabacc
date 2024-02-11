5f9e9beca567471e25c40c95929fabac2053a63b
Aidan-Kelley~ <aidank6@yahoo.com>
2024-02-10 22:11:28 -0800

    simulation class started

--- src/main/Simulation.java
+++ src/main/Simulation.java
@@ -1,9 +1,34 @@
 package main;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Simulation {
-    public Simulation() {
-        Game game = new Game(4);
-        int winner = game.runGame();
-        System.out.println(game);
-        System.out.println(winner + 1);
+
+    private Game game;
+    private int winner;
+
+    public Simulation(int trials) {
+
+        for (int i = 0; i < trials; i++) {
+            game = new Game(2);
+            winner = game.runGame();
+            logGame(game.toString(), winner);
+        }
+    }
+
+    
+
+    private void logGame(String hands, int winner) {
+        try {
+            FileWriter writer = new FileWriter("src\\logs\\games.txt", true);
+            writer.write(hands);
+            writer.write("Winner: " + Integer.toString(winner));
+            writer.write("\n\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        } 
     }
 }
